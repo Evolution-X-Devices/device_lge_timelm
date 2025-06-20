ec2aafd9df6495bd356bc97b72a8c9a8f8fbc0c7
LuK1337 <priv.luk@gmail.com>
2025-06-20 14:45:11 +0700

    sm8250-common: Move WfdCommon boot jar declaration to vendor
    Change-Id: I8fa2c0f64692873b432cc252ec88b388a32023a1

--- sm8250.mk
+++ sm8250.mk
@@ -437,10 +437,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
-# WiFi Display
-PRODUCT_BOOT_JARS += \
-    WfdCommon
-
 # WiFi firmware symlinks
 PRODUCT_PACKAGES += \
     qca6390_firmware_symlink \
