fd406e8df2c85c605aabb908f51e0aee879d73c5
pnguyen879 <howardson2001@gmail.com>
2025-09-05 20:51:21 -0500

    timelm: Drop manifest_odm.xml
    This doesn't do anything for our device's display
    
    Change-Id: I60cbf3e615c6fd078bec41067ce04bdf4d06eef2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -70,7 +70,6 @@ $(call soong_config_set,LGE_FINGERPRINT_HAL,TARGET_HAS_EGISTEC_UDFPS,true)
 # HIDL
 DEVICE_MATRIX_FILE += hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
-ODM_MANIFEST_FILES += $(DEVICE_PATH)/manifest_odm.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
