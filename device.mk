a2c2d8b00799b8c7453a482865cacc4d235f9585
pnguyen879 <howardson2001@gmail.com>
2025-10-04 12:31:14 -0500

    timelm: Configure LiveDisplay AIDL
    Change-Id: I9c8c3e4ebd1d9608a3aa0971631b58c8e1b3c5e2

--- device.mk
+++ device.mk
@@ -204,6 +204,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay-service.lge
 
+$(call soong_config_set,livedisplay_lge,enable_se,true)
+$(call soong_config_set,livedisplay_lge,enable_ce,true)
+
 # Media
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/media/,$(TARGET_COPY_OUT_VENDOR)/etc) \
