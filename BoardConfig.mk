41030bbc307c07aa7e77fa80ec19b1f5d9d3bb66
pnguyen879 <howardson2001@gmail.com>
2026-05-11 20:43:31 -0500

    timelm: Use hardware/lge device_framework_matrix.xml
    Change-Id: I3793e6e6223ad7bf30933dcdc340ca42a6ec9f5d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -61,7 +61,7 @@ TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 DEVICE_MATRIX_FILE += hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
-    $(DEVICE_PATH)/device_framework_matrix.xml \
+    hardware/lge/vintf/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
 
 # Kernel
